715fe3aff0b93d08b0a45e107c8d4cd0b7630814
KevindeMeijer <55430418+KevindeMeijer@users.noreply.github.com>
2020-10-15 13:24:09 +0200

    :tada: Start of learning

--- src/index.js
+++ src/index.js
@@ -1,92 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-//
-//
+import Flex from "./flex.js";
 
-const Header = (props) => {
-  return(
-      <div className='card-header'>
-        <h1 className='card-header-title header'>
-          You have {props.numTodos} To-do's
-        </h1>
-      </div>
-  )
+import "./flex.css";
+
+class Homepage extends React.Component {
+	render() {
+		return <h1></h1>;
+	}
 }
 
-const TodoList = (props) => {
-  const todos = props.tasks.map((todo, index) => {
-    return <Todo content={todo} key={index} id={index} onDelete={props.onDelete} />
-  })
-  return(
-      <div className='list-wrapper'>
-        {todos}
-      </div>
-  );
-}
-
-const Todo = (props) => {
-  return(
-      <div className='list-item'>
-        {props.content}
-        <button class="delete is-pulled-right" onClick={() => {props.onDelete(props.id)}}></button>
-      </div>
-  );
-}
-
-class SubmitForm extends React.Component {
-  state = { term: '' };
-  handleSubmit = (e) => {
-    e.preventDefault();
-    if(this.state.term === '') return;
-    this.props.onFormSubmit(this.state.term);
-    this.setState({ term: '' });
-  }
-  render() {
-    return(
-        <form onSubmit={this.handleSubmit}>
-          <input
-              type='text'
-              className='input'
-              placeholder='Enter Item'
-              value={this.state.term}
-              onChange={(e) => this.setState({term: e.target.value})}
-          />
-          <button className='button'>Submit</button>
-        </form>
-    );
-  }
-}
-
-class ToDoList extends React.Component {
-  state = {
-    tasks: ['task 1', 'task 2', 'task 3', 'task 4']
-  };
-
-  handleDelete = (index) => {
-    const newArr = [...this.state.tasks];
-    newArr.splice(index, 1);
-    this.setState({tasks: newArr});
-  }
-
-  handleSubmit = task => {
-    this.setState({tasks: [...this.state.tasks, task]});
-  }
-
-  render() {
-    return(
-        <div className='wrapper'>
-          <div className='card frame'>
-            <Header numTodos={this.state.tasks.length} />
-            <TodoList tasks={this.state.tasks} onDelete={this.handleDelete}/>
-            <SubmitForm onFormSubmit={this.handleSubmit} />
-          </div>
-        </div>
-    );
-  }
-}
-
-ReactDOM.render(
-    <ToDoList />,
-    document.querySelector('#root')
-);
+ReactDOM.render(<Homepage />, document.querySelector("#root"));
